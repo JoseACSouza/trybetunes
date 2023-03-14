c76fbeb77a3177db3bd063ae8e38d9dab9619f2c
José Alexandre Cordeiro Souza <alexandrecordeiro15@outlook.com>
2023-03-13 21:17:01 -0300

    cumpre o primeiro requisito

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import React from 'react';
-//comentário para o commit inicial
+import { Route, Switch } from 'react-router-dom';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import NotFound from './pages/NotFound';
+import ProfileEdit from './pages/ProfileEdit';
+import Profile from './pages/Profile';
+
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <main>
+        <Switch>
+          {/* Quando estiver na rota / renderize: Login */}
+          <Route exact path="/" component={ Login } />
+
+          <Route exact path="/search" component={ Search } />
+          <Route exact path="/album/:id" component={ Album } />
+          <Route exact path="/favorites" component={ Favorites } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/profile/edit" component={ ProfileEdit } />
+
+          {/* Se não encontrar nenhuma rota até aqui, renderize: NotFound */}
+          <Route path="*" component={ NotFound } />
+        </Switch>
+      </main>
+    );
   }
 }
 
