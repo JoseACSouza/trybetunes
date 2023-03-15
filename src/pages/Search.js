e5927114c709b7daa799132bb3181f74a19f8978
José Alexandre Cordeiro Souza <alexandrecordeiro15@outlook.com>
2023-03-14 21:37:52 -0300

    cumpre requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,10 +2,49 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      inputSearch: '',
+      handleButton: true,
+    };
+  }
+
+  verifyLength = () => {
+    const { inputSearch } = this.state;
+    const minLength = 2;
+    this.setState({
+      handleButton: inputSearch.length < minLength,
+    });
+  };
+
+  inputChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    }, this.verifyLength);
+  };
+
   render() {
+    const { inputSearch, handleButton } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            value={ inputSearch }
+            onChange={ this.inputChange }
+            name="inputSearch"
+          />
+          <button
+            data-testid="search-artist-button"
+            disabled={ handleButton }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
