c3e90942038fe058a314da25cf55a9b21ca8b41e
José Alexandre Cordeiro Souza <alexandrecordeiro15@outlook.com>
2023-03-14 21:25:52 -0300

    cumpre requisito 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -31,6 +32,15 @@ class Header extends React.Component {
         <h1 data-testid="header-user-name">
           { userName }
         </h1>
+        <Link to="/search" data-testid="link-to-search">
+          Search
+        </Link>
+        <Link to="/favorites" data-testid="link-to-favorites">
+          Favorites
+        </Link>
+        <Link to="/profile" data-testid="link-to-profile">
+          Profile
+        </Link>
       </header>
     );
   }
