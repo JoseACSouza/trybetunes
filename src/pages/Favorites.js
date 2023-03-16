394604b9bc2cabc7000df964421aa66e8209d4e1
José Alexandre Cordeiro Souza <alexandrecordeiro15@outlook.com>
2023-03-16 11:55:47 -0300

    cumpre requisito 13

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,11 +1,78 @@
 import React from 'react';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import MusicCard from '../components/MusicCard';
+import { addSong, removeSong, getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 class Favorites extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: true,
+      musicList: [],
+      isChecked: [],
+    };
+  }
+
+  componentDidMount() {
+    this.getSongList();
+  }
+
+  onInputChange = (event) => {
+    const { name, checked } = event.target;
+    const { musicList } = this.state;
+    this.setState({
+      isLoading: true,
+    }, async () => {
+      if (checked) {
+        await addSong(musicList
+          .find((song) => song.trackId === parseInt(name, 10)));
+      } else {
+        await removeSong(musicList
+          .find((song) => song.trackId === parseInt(name, 10)));
+      }
+      await this.getSongList();
+      this.setState({
+        isLoading: false,
+      });
+    });
+  };
+
+  getSongList = async () => {
+    this.setState({
+      isLoading: true,
+    });
+    const songList = await getFavoriteSongs();
+    this.setState({
+      isLoading: false,
+      isChecked: songList.filter((item) => item.trackId).map((song) => song.trackId),
+      musicList: songList.filter((item) => item.trackId),
+    });
+  };
+
   render() {
+    const { isLoading, musicList } = this.state;
     return (
       <div data-testid="page-favorites">
         <Header />
+        {isLoading ? <Loading /> : (
+          <>
+            { musicList.map((music) => {
+              const { isChecked } = this.state;
+              return (
+                <div key={ music.trackId }>
+                  <MusicCard
+                    trackId={ music.trackId }
+                    trackName={ music.trackName }
+                    previewUrl={ music.previewUrl }
+                    onInputChange={ this.onInputChange }
+                    isChecked={ isChecked }
+                  />
+                </div>
+              );
+            }) }
+          </>
+        )}
       </div>
     );
   }
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,47 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import { getUser } from '../services/userAPI';
 
 class Profile extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: true,
+      userInfo: {},
+    };
+  }
+
+  componentDidMount() {
+    this.getUserInfo();
+  }
+
+  getUserInfo = async () => {
+    const userInfo = await getUser();
+    this.setState({
+      isLoading: false,
+      userInfo,
+    });
+  };
+
   render() {
+    const { userInfo, isLoading } = this.state;
     return (
       <div data-testid="page-profile">
         <Header />
+        {isLoading ? <Loading /> : (
+          <>
+            <p>{userInfo.name}</p>
+            <p>{userInfo.email}</p>
+            <p>{userInfo.description}</p>
+            <img src={ userInfo.image } alt="avatar" data-testid="profile-image" />
+
+            <Link to="/profile/edit">
+              Editar perfil
+            </Link>
+          </>
+        )}
       </div>
     );
   }
